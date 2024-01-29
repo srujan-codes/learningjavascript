accb590503e18b84b902af901e1971b62d3ce61f
srujancodes <vooturisrujan@gmail.com>
2024-01-29 08:00:39 +0530

    learnt const and var

--- variables.js
+++ variables.js
@@ -11,3 +11,10 @@ accountEmail = "srujan2@gmail.com"
 accountPassword = "password2"
 accountCity = "Bangalore"
 console.table([accountEmail,accountPassword,accountId,accountPassword,accountCity])
+/*
+prefer not to use var because of issue in block scope and functional scope
+Q: what is block scope?
+A: Block scope is often used in conditional statements and loops to control the scope of variables.
+Q: what is functional scope?
+A: Variables that are declared inside a function are called local variables and in the function scope.
+*/
