63e1729ea3f115005cb52dc354612bb402b125b4
srujancodes <vooturisrujan@gmail.com>
2024-01-30 07:45:50 +0530

    Operations

---
+++ 07strings.js
@@ -0,0 +1,19 @@
+const name = "srujan"
+const age = 20
+console.log(name + age)
+
+
+dvcfdfvvvvvvvvvv
+
+
+
+
+
+
+
+/*
+Summary
+strings can be denoted with 'single' or "double quotes"  and do not require any special characters. 
+Strings are used to store text data in programming languages.
+
+*/
