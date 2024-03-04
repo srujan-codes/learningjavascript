12c2312adf7c5fb3b9394630ceb7461905843099
srujancodes <vooturisrujan@gmail.com>
2024-03-04 18:23:41 +0530

    functionsAndParametersInJs

--- 15functionsAndParametersInJs.js
+++ 15functionsAndParametersInJs.js
@@ -24,16 +24,16 @@ sayMyName()   // calling the function by referencing its name
     console.log(number1 + number2);
 
 } */
- addTwoNumbers() // returned NaN (Not A Number)
- addTwoNumbers(3,8) // returned 11   // things inside  () are called arguments
- addTwoNumbers(3,"8") // returned 38 because it assumed 3 also as a string
- addTwoNumbers(3,"a") // returned 3a
+ //addTwoNumbers() // returned NaN (Not A Number) because we haven't given any agument
+ //addTwoNumbers(3,8) // returned 11   // things inside  () are called arguments
+// addTwoNumbers(3,"8") // returned 38 because it assumed 3 also as a string
+ //addTwoNumbers(3,"a") // returned 3a
  
 
 
 function addTwoNumbers (number1, number2){
     //let result = number1 + number2
-   // return result  // after return function, whatever there in the function won't get executed.
+   // return result  // after return function, whatever there in the function won't get executed. after return ,function will not work.node 
 return number1+number2 // can also be done
 }
 
@@ -41,19 +41,27 @@ const result = addTwoNumbers(3, 5)
 
 console.log("Result: ", result); // returned 8 
 
-function loginUserMessage(username = "srujan"){ // here srujan is default value 
-    if(username === undefined){   // here we could also use !username        
+function loginUserMessage(username = "srujan"){ // here srujan is default value . We've assigned default value
+    if(username === undefined){   // here we could also use !username and we did this so we did not want to leave it to be undefined    
         console.log("Please enter your username.")
         return
 
     }
-    return `${username}  is logged in.`
+    return `${username}  is logged in.`    // (``)==>String interpolation back ticks is used 
 }
+
 
 
 //console.log(loginUserMessage("srujan")) // returned srujan  is logged in.
 //console.log(loginUserMessage("")) // returned  is logged in. in the case of empty string
-console.log(loginUserMessage()) // returned undefined  is logged in.
+console.log(loginUserMessage()) // returned undefined  is logged in. because we did not pass any command
 
 
-// empty string and undefined are by default fault valeus or false
+// empty string and undefined are by default fault values or false
+
+
+/*
+Interpolation of variables refers to the process of embedding variable values directly into a string, 
+typically within template literals in JavaScript, using placeholders like ${variable}. 
+This allows for dynamic content generation within strings without the need for cumbersome concatenation or string manipulation.
+ */
