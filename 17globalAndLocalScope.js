45f37ee776a22c92a9c60cde735f740ea9b97f33
srujancodes <vooturisrujan@gmail.com>
2024-03-05 07:49:01 +0530

    Global And Local Scope

--- 17globalAndLocalScope.js
+++ 17globalAndLocalScope.js
@@ -15,3 +15,28 @@ if(true){
 }
 
 console.log(a); 
+
+/*
+              Block Scope 
+    let and const provide block scope in javascript.
+     Variables declared inside a { } block cannot be accessed from outside the block.       
+     variables declared with the "var" keyword can NOT have block scope.   
+     Variables declared inside a { } block can be accessed from outside the block.
+
+              Local Scope
+    Variables declared within a JavaScript function, are LOCAL to the function
+    // code here can NOT use carName
+
+function myFunction() {
+  let carName = "Volvo";
+  // code here CAN use carName
+}
+
+// code here can NOT use carName
+
+Since local variables are only recognized inside their functions, variables with the same name can be used in different functions.
+Local variables are created when a function starts, and deleted when the function is completed.
+
+                   Global JavaScript Variables
+A variable declared outside a function, becomes GLOBAL.
+*/
